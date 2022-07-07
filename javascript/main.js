5d7cb4f38909233f3d0d80ebee1289c2208bd502
ellie <ellie2022.nova@gmail.com>
2022-07-07 16:48:05 +0900

    javascript part added

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+// added in ECMAScript 5
+'use strict';
+    console.log("Hello World!");
+    console.warn("Watch out!");
+    console.error("Oops!!");
+    document.write("Hello!");
+    const char = 'c';
+    const brendan = "brendan";
+    const greeting = 'hello ' + brendan;
+    console.log(`value: ${greeting}, type: ${typeof greeting}`);
+    const helloBob = `hi ${brendan}! `;
+    console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+    console.log('value: ' + helloBob + ' type: '+ typeof helloBob);
+
+
+    
